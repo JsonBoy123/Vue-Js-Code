9d804bf46869a035f415ae37df653f077bb2b602
Json Boy <jsonboy123@gmail.com>
2022-02-07 18:07:12 +0530

    changes in home.vue components file

---
+++ src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+<h1>First Home Component {{10+20}}</h1>
+<h1>Email {{email}}</h1>
+<h1>Name {{getName()}}</h1>
+<h1>Name {{setName()}}</h1>
+<h1>All Data {{getData().email}}</h1>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+            email: 'abc@test.com',
+            getName: function () {
+                return "ABC"
+            }
+        }
+    },
+    methods: {
+        setName() {
+            return "DBF_POS"
+        },
+        getData() {
+            return {
+                name: "sam",
+                email: this.email
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: #fa0000;
+    font-style: italic;
+}
+</style>
